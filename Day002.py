6284b92b350f2e7709986609e441dadb9c386db9
amitvsolutions <amitverma.ibmindia@gmail.com>
2023-03-05 21:04:48 +0530

    strings in python

---
+++ Day002.py
@@ -0,0 +1,11 @@
+#Create String in 3 ways
+message = "Hello Amit"
+print(message)
+
+# second way
+mesg= 'We are going to learn "Python"'
+print(mesg)
+
+# third way: in case we need "" and '' in our string, then use """ or '''
+msg = """ "Learning is Earning" and that's '100%' true."""
+print(msg)
