447e7f32e810b59d052444ac4cddc5da1c57bb91
amitvsolutions <amitverma.ibmindia@gmail.com>
2023-03-12 19:02:22 +0530

    boolean in python

---
+++ Day007.py
@@ -0,0 +1,20 @@
+# boolean in python [TRUE or FALSE]
+# if withdrwal_amount > balance : don't allow withdraw
+
+# Waeather app- if raining: grab umbrella else grab sunglasses
+
+weather = input('How is the waether?: ')
+
+if weather == 'rain':
+    print('Grab an Umbrella ')
+elif weather == 'cold':
+    print('Grab a Jacket ')
+else:
+    print('Grab sunglasses')
+
+# To check type of a vraible
+type(weather)
+type(5.6)
+
+# Exercise: Grade App [score >=90 A, >=80 B, >=70 C, >=60 D, <=60 F]
+
